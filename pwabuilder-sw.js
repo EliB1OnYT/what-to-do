9f1591420dcedd76c1f8d9c8c4f5601c7992b29b
EliB1OnYT <161958116+EliB1OnYT@users.noreply.github.com>
2025-09-19 13:29:06 -0400

    pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,11 +1,8 @@
 // This is the "Offline page" service worker
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html"; // <-- Make sure offline.html exists in your root
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -16,7 +13,7 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => cache.addAll([offlineFallbackPage, '/index.html'])) // Cache offline page + main page
   );
 });
 
@@ -28,16 +25,15 @@ self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
     event.respondWith((async () => {
       try {
+        // Try to use preload response if available
         const preloadResp = await event.preloadResponse;
+        if (preloadResp) return preloadResp;
 
-        if (preloadResp) {
-          return preloadResp;
-        }
-
+        // Otherwise, try network
         const networkResp = await fetch(event.request);
         return networkResp;
       } catch (error) {
-
+        // If network fails, serve cached offline page
         const cache = await caches.open(CACHE);
         const cachedResp = await cache.match(offlineFallbackPage);
         return cachedResp;
